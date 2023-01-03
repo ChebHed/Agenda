92520fb09033c143095d1e7262c50a88247ad66a
Fesses <Fesses@DESKTOP-K76EAP9>
2023-01-03 17:50:55 +0100

    Ajoutez des fichiers projet.

---
+++ Model/Event.cs
@@ -0,0 +1,12 @@
+﻿namespace Agenda.Model
+{
+    public class Event
+    {
+        public DateTime Date { get; set; }
+        public TimeSpan Time { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+    }
+
+
+}
